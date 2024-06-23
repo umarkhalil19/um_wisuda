92aee53d639bbbe227479fdac983d931caaee4e2
arc-venom <55178976+umarkhalil19@users.noreply.github.com>
2024-06-23 21:46:19 +0700

    feat: update syarat wisuda

--- application/views/wisuda/v_home.php
+++ application/views/wisuda/v_home.php
@@ -8,6 +8,9 @@
 			<li class="breadcrumb-item"><a href="#">Dashboard</a></li>
 		</ul>
 	</div>
+	<?php
+	show_alert();
+	?>
 	<div class="row">
 		<div class="col-md-12">
 			<div class="tile">
@@ -15,21 +18,21 @@
 				<div align="center">
 					<img style="align-items: center" width="10%" src="assets/Logo_Unimal.png">
 				</div>
-				<?php 
-                	if ($psw == 'ganti') {
-                		echo 
-                		'<div align="center">
+				<?php
+				if ($psw == 'ganti') {
+					echo
+					'<div align="center">
 							 <label class="alert alert-danger"><strong>Sistem Mendeteksi Anda Login Dengan Menggunakan Password Default, Harap Diubah Terlebih Dahulu.<br>Silahkan Klik Tombol Ganti Password Untuk Merubah Password Default Anda</strong></label><br>
-							 <a href="'.base_url('wisuda/password').'" class="btn btn-primary"><strong>Ganti Password</strong></a>
+							 <a href="' . base_url('wisuda/password') . '" class="btn btn-primary"><strong>Ganti Password</strong></a>
 						 </div>';
-                	}else{
-                		echo 
-                		'<div align="center">
+				} else {
+					echo
+					'<div align="center">
 							 <br>
-							 <a href="'.base_url('wisuda/daftarwisuda').'" class="btn btn-primary"><strong>DAFTAR WISUDA</strong></a>
+							 <a href="' . base_url('wisuda/daftarwisuda') . '" class="btn btn-primary"><strong>DAFTAR WISUDA</strong></a>
 						 </div>';
-                	}
-				 ?>
+				}
+				?>
 			</div>
 		</div>
 	</div>
