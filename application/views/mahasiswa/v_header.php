92df26f64adad512f1a3f10b4131f0425dcf8bbb
arc-venom <umarkhalil19@gmail.com>
2025-09-17 16:30:06 +0700

    update : tambah kode motomo untuk web analytic

--- application/views/mahasiswa/v_header.php
+++ application/views/mahasiswa/v_header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta name="description" content="<?php echo $this->M_vic->get_option('blog_name').' '.$this->M_vic->get_option('blog_description'); ?>">
-  <link rel="shortcut icon" href='<?php echo base_url()?>vic_image/system/logo.png'>
+  <meta name="description" content="<?php echo $this->M_vic->get_option('blog_name') . ' ' . $this->M_vic->get_option('blog_description'); ?>">
+  <link rel="shortcut icon" href='<?php echo base_url() ?>vic_image/system/logo.png'>
   <title><?php echo $this->M_vic->get_option('blog_name'); ?></title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,48 +11,71 @@
   <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/main.css">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/drago19.css">
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script type="text/javascript" src="<?php echo base_url().'assets/plugin/ckeditor/ckeditor.js' ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url() . 'assets/plugin/ckeditor/ckeditor.js' ?>"></script>
+  <!-- Matomo -->
+  <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u = "https://analytics.unimal.ac.id/";
+      _paq.push(['setTrackerUrl', u + 'matomo.php']);
+      _paq.push(['setSiteId', '14']);
+      var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0];
+      g.async = true;
+      g.src = u + 'matomo.js';
+      s.parentNode.insertBefore(g, s);
+    })();
+  </script>
+  <!-- End Matomo Code -->
 </head>
+
 <body class="app sidebar-mini rtl">
-<?php
+  <?php
   $db = $this->M_vic->panggil_db();
   $id = $this->session->userdata('uid');
   $pes = mysqli_query($db, "SELECT * FROM tbl_pesan WHERE tp_mahasiswa = '$id' AND (tp_status_baca = '' OR tp_status_baca = 'Belum Dibaca') AND h_pengguna !='$id'");
   $jml = mysqli_num_rows($pes);
   $tgl = date_create('Y-m-d');
   $fa = 'fa fa-bell-o fa-lg';
-  if($jml > 0){ $fa = 'fa fa-bell fa-lg';
-  }else{ $fa = 'fa fa-bell-o fa-lg'; }
-?>
+  if ($jml > 0) {
+    $fa = 'fa fa-bell fa-lg';
+  } else {
+    $fa = 'fa fa-bell-o fa-lg';
+  }
+  ?>
 
   <!-- Navbar-->
-  <header class="app-header"><a class="app-header__logo" href="<?php echo base_url().'mahasiswa' ?>" style="font-family: times new roman">UNIMAL</a>
+  <header class="app-header"><a class="app-header__logo" href="<?php echo base_url() . 'mahasiswa' ?>" style="font-family: times new roman">UNIMAL</a>
     <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
     <ul class="app-nav">
       <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="<?php echo $fa; ?>"></i></a>
         <ul class="app-notification dropdown-menu dropdown-menu-right">
-          <?php 
-          echo '<li class="app-notification__title">Anda memiliki '.$jml.' Pesan baru.</li>';
+          <?php
+          echo '<li class="app-notification__title">Anda memiliki ' . $jml . ' Pesan baru.</li>';
           echo '<div class="app-notification__content">';
           while ($p = mysqli_fetch_array($pes)) {
-          echo '<li><a class="app-notification__item" href="'.base_url('mahasiswa/pesan_tampil/'.$p['tp_kode']).'"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span>';
-          echo '<div>';
-            echo '<p class="app-notification__message">'.$p['tp_admin'].' mengirim pesan</p>';
-            echo '<p class="app-notification__meta">Pada '.$p['h_tanggal'].' Jam '.$p['h_waktu'].'</p>';
+            echo '<li><a class="app-notification__item" href="' . base_url('mahasiswa/pesan_tampil/' . $p['tp_kode']) . '"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span>';
+            echo '<div>';
+            echo '<p class="app-notification__message">' . $p['tp_admin'] . ' mengirim pesan</p>';
+            echo '<p class="app-notification__meta">Pada ' . $p['h_tanggal'] . ' Jam ' . $p['h_waktu'] . '</p>';
           }
           echo '</div></a>';
           echo '</li>';
           echo '</div>';
-          echo '<li class="app-notification__footer"><a href="'.base_url().'mahasiswa/pesan">Lihat semua.</a></li>'; 
+          echo '<li class="app-notification__footer"><a href="' . base_url() . 'mahasiswa/pesan">Lihat semua.</a></li>';
           ?>
         </ul>
-        
+
       </li>
-      <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i> &nbsp;  <?php echo $this->session->userdata('unama') ?></a>
-          <ul class="dropdown-menu settings-menu dropdown-menu-right">
-            <li><a class="dropdown-item" href="<?php echo base_url('login/logout') ?>"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
-          </ul>
-        </li>
+      <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i> &nbsp; <?php echo $this->session->userdata('unama') ?></a>
+        <ul class="dropdown-menu settings-menu dropdown-menu-right">
+          <li><a class="dropdown-item" href="<?php echo base_url('login/logout') ?>"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+        </ul>
+      </li>
       <!-- User Menu-->
     </ul>
   </header>
@@ -65,11 +89,11 @@
       </div>
     </div>
     <ul class="app-menu">
-      <li><a class="app-menu__item" href="<?php echo base_url().'mahasiswa' ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+      <li><a class="app-menu__item" href="<?php echo base_url() . 'mahasiswa' ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
       <?php if ($psw == 'oke') { ?>
-      <li><a class="app-menu__item" href="<?php echo base_url().'mahasiswa/biodata' ?>"><i class="app-menu__icon fa fa-pencil"></i><span class="app-menu__label">Data Diri</span></a></li>
-      <li><a class="app-menu__item" href="<?php echo base_url().'mahasiswa/alur' ?>"><i class="app-menu__icon fa fa fa-check"></i><span class="app-menu__label">Proses Pengurusan</span></a></li>
-      <li><a class="app-menu__item" href="<?php echo base_url().'mahasiswa/pesan' ?>"><i class="app-menu__icon fa fa-envelope"></i><span class="app-menu__label">Pesan</span></a></li>
+        <li><a class="app-menu__item" href="<?php echo base_url() . 'mahasiswa/biodata' ?>"><i class="app-menu__icon fa fa-pencil"></i><span class="app-menu__label">Data Diri</span></a></li>
+        <li><a class="app-menu__item" href="<?php echo base_url() . 'mahasiswa/alur' ?>"><i class="app-menu__icon fa fa fa-check"></i><span class="app-menu__label">Proses Pengurusan</span></a></li>
+        <li><a class="app-menu__item" href="<?php echo base_url() . 'mahasiswa/pesan' ?>"><i class="app-menu__icon fa fa-envelope"></i><span class="app-menu__label">Pesan</span></a></li>
       <?php } ?>
     </ul>
   </aside>
